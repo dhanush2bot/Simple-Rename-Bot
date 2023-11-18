938a051748cc9df2dda8b106a563512d47216f83
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-18 18:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "23081466"))
 API_HASH = environ.get("API_HASH", "dbc665db1489f9d3cfd8de4a52f1ad4b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6311317123:AAHcqbM9s6MwjQd-5ToPLyD4AWhv16PNV40")
 ADMIN = int(environ.get("ADMIN", "1764208280"))          
-CAPTION = environ.get("CAPTION", "")
+CAPTION = environ.get("CAPTION", "[ @filmyspotmovie ] {file_name}")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
